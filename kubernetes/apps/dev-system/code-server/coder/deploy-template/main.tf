d468d305602eb5eedea7c7fa06bacfbfae2c3adc
imerkle <dsslimshaddy@gmail.com>
2026-03-07 23:43:34 +0530

    t

--- kubernetes/apps/dev-system/code-server/coder/deploy-template/main.tf
+++ kubernetes/apps/dev-system/code-server/coder/deploy-template/main.tf
@@ -1,26 +1,23 @@
 terraform {
   required_providers {
-    coder = {
-      source  = "coder/coder"
-      version = "~> 0.23.0"
+    coderd = {
+      source  = "coder/coderd"
+      version = "~> 1.0"
     }
   }
 }
 
-provider "coder" {
+provider "coderd" {
   # Provider will be configured via CODER_URL and CODER_SESSION_TOKEN environment variables
 }
 
-resource "coder_template_version" "kubernetes_dev" {
-  directory = "${path.module}/../template"
-}
+resource "coderd_template" "kubernetes_dev" {
+  name        = "kubernetes-dev"
+  description = "A standard development environment running inside a Kubernetes Pod."
 
-resource "coder_template" "kubernetes_dev" {
-  name         = "kubernetes-dev"
-  display_name = "Kubernetes Developer Workspace"
-  description  = "A standard development environment running inside a Kubernetes Pod."
-
-  versions {
-    active_id = coder_template_version.kubernetes_dev.id
-  }
+  versions = [
+    {
+      directory = "${path.module}/../template"
+    }
+  ]
 }
