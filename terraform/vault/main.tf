d5ac4c7b08c7f81dfa3b8b6d609e582a6e80a17e
imerkle <dsslimshaddy@gmail.com>
2026-06-21 22:43:51 +0530

    refactor: migrate applications to use Vault database dynamic secrets and remove ExternalSecret resources

--- terraform/vault/main.tf
+++ terraform/vault/main.tf
@@ -25,33 +25,5 @@ provider "vault" {
   token   = data.kubernetes_secret_v1.vault_unseal_keys.data["vault-root"]
 }
 
-locals {
-  static_roles = [
-    { name = "synapse-static", username = "synapse_user" },
-    { name = "mas-static", username = "mas_user" },
-    { name = "atuin-static", username = "atuin_user" },
-    { name = "vaultwarden-static", username = "vaultwarden_user" },
-    { name = "zealot-static", username = "zealot_user" },
-    { name = "zitadel-static", username = "zitadel_user" },
-    { name = "forgejo-static", username = "forgejo_user" },
-    { name = "coder-static", username = "coder_user" },
-    { name = "litellm-static", username = "litellm_user" },
-    { name = "keto-static", username = "keto_user" },
-    { name = "temporal-static", username = "temporal_user" },
-    { name = "game-fetcher-static", username = "game_fetcher_user" },
-    { name = "settings-server-static", username = "settings_server_user" },
-    { name = "mautrix-discord-static", username = "mautrix_discord_user" },
-    { name = "mautrix-whatsapp-static", username = "mautrix_whatsapp_user" },
-    { name = "mautrix-telegram-static", username = "mautrix_telegram_user" }
-  ]
-}
 
-resource "vault_database_secret_backend_static_role" "roles" {
-  for_each            = { for r in local.static_roles : r.name => r }
-  backend             = "database"
-  name                = each.value.name
-  db_name             = "pg-default"
-  username            = each.value.username
-  rotation_period     = "604800" # 7 days in seconds
-  rotation_statements = ["ALTER ROLE \"{{name}}\" WITH PASSWORD '{{password}}';"]
-}
+
